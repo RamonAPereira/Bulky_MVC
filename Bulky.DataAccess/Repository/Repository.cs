9ae8075535e5c849379f6c9db97760f6ab9a69d5
RamonAPereira <ramonpereiranzd@gmail.com>
2024-03-19 19:10:30 -0300

    feat: CategoryRepository implementation

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> DbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.DbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = DbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = DbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void removeRange(IEnumerable<T> entity)
+        {
+            DbSet.RemoveRange(entity);
+        }
+    }
+}
